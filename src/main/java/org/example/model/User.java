0a3ef0fb79fb5f2aa4e0dfd3d03f26c8930d9a18
hassankoroma <stomata-studded-7f@iclound.com>
2025-07-01 18:55:18 +0900

    error message

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,72 +1,32 @@
 package org.example.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
-    @NotBlank(message = "First name is required")
-    @Column(name = "first_name")
-    private String firstName;
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String name;
 
-    @NotBlank(message = "Last name is required")
-    @Column(name = "last_name")
-    private String lastName;
-
-    @NotBlank(message = "Email is required")
-    @Email(message = "Please enter a valid email address")
-    @Column(name = "email")
+    @NotNull
+    @Email
     private String email;
 
-    // Default constructor required by Hibernate
-    public User() {
-    }
-
-    public User(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    // Getters and Setters
-    // ...
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    // Getters and setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
